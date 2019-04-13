9b08f9c3d94a4f9dc4d4aad0ac019312d76be9fa
Adam Daskalakis <adamdaskalakis@gmail.com>
2019-04-13 17:29:04 -0400

    New Stuff

--- contact.php
+++ contact.php
@@ -6,7 +6,24 @@ if(is_mobile()){
 } else {
   $HTML = file_get_contents('main_temp.html');
   $HTML = str_replace('{NAVBAR}', get_navbar(), $HTML);
-  $MAIN = "THIS IS THE CONTACT PAGE.  FORM NEEDED!";
+  $MAIN = '<form id="contact">CONTACT US FOR A QUOTE
+  <br>
+  <br>
+  <input type="text" name="name" placeholder="Name">
+  <br>
+  <input type="email" name="email" placeholder="Email">
+  <br>
+  <input type="text" name="site" placeholder="Website Address">
+  <br>
+  <input type="text" name="project" placeholder="Project Type" class="short">
+  <input type="text" name="budget" placeholder="Budget" class="short">
+  <br>
+  <textarea placeholder="Tell us more..."></textarea>
+  <br>
+  <input type="date" name="date" placeholder="Launch Date" class="short">
+  <br>
+  <input type="submit" value="Send Email">
+  </form>';
   $HTML = str_replace('{MAIN}', $MAIN, $HTML);
 }
 $HTML = str_replace('{RND}', rand(), $HTML);
